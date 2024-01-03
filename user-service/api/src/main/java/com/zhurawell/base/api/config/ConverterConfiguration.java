0916627ed44c98a743028d281fde19596636c21b
Demon3003 <zhuravlyovdima34@gmail.com>
2024-01-03 19:57:24 +0200

    Rename folder for user-service

--- user-service/api/src/main/java/com/zhurawell/base/api/config/ConverterConfiguration.java
+++ user-service/api/src/main/java/com/zhurawell/base/api/config/ConverterConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class RestConfiguration implements WebMvcConfigurer {
+public class ConverterConfiguration implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new UserRestConverter());
